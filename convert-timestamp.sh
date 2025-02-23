5da065872fcd76df88978a40034c161b475f532d
Sean Sovine <sean.r.sovine@gmail.com>
2025-02-23 17:28:08 -0500

    Add explanatory comment to script

--- convert-timestamp.sh
+++ convert-timestamp.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
-# Suggestion from Google AI>
-
-#timestamp=1678886400
+# Proper unix timestamp.
 timestamp=1694101648
+# Timestamp from JSON file.
+timestamp=1694104295853
+
+# Approach suggestion from Google AI:
 formatted_date=$(date -d "@$timestamp" +"%Y-%m-%d %H:%M:%S")
 echo $formatted_date
+
+## NOTE: We used this to figure out that the timestamps in
+## the file are in milliseconds. We are timestamps expecting
+## in seconds in our Rust code, so we will convert.
